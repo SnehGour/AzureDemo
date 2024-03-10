00ba039316bb54a06cd24ba12d10af85702c5c84
snehgour80@gmail.com <snehgour80@gmail.com>
2024-03-10 08:51:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.ServiceBus;
+using System.Text;
+
+const string connectionString = "Endpoint=sb://snehservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=naclyROzXr3JZMOoLCsFS7PPBs+KDXvqD+ASbPso99E=";
+const string queueName = "snehqueue";
+IQueueClient queueClient = new QueueClient(connectionString,queueName);
+Message message = new Message(Encoding.UTF8.GetBytes("Hello"));
+
+await queueClient.SendAsync(message);
+Console.WriteLine("Send");
